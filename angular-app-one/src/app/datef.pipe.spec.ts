2580d4992e879cbc306ac5d46e68cb24585ca6b4
Carl <ldscode@163.com>
2017-08-27 13:42:06 +0800

    remove test code

--- angular-app-one/src/app/datef.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DatefPipe } from './datef.pipe';
-
-describe('DatefPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DatefPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
