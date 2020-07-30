f13bfafe72a252f55e1b462319e8527122222cfa
Carl-DS <ldscode@163.com>
2020-07-30 15:26:58 +0800

    add: progress

---
+++ ds-cloud-music/src/application/Player/store/index.js
@@ -0,0 +1,5 @@
+import reducer from './reducer'
+import * as actions from './actions'
+import * as actionTypes from './action-types'
+
+export { reducer, actions, actionTypes };
--- ds-cloud-music/src/store/reducer.js
+++ ds-cloud-music/src/store/reducer.js
@@ -4,11 +4,13 @@ import { reducer as SingersReducer } from '../application/Singers/store/index';
 import { reducer as rankReducer } from '../application/Rank/store/index';
 import { reducer as albumReducer } from '../application/Album/store/index';
 import { reducer as singerInfoReducer } from "../application/Singer/store/index";
+import { reducer as playerReducer } from "../application/Player/store/index";
 
 export default combineReducers({
   recommend: recommendReducer,
   singers: SingersReducer,
   rank: rankReducer,
   album: albumReducer,
-  singerInfo: singerInfoReducer
+  singerInfo: singerInfoReducer,
+  player: playerReducer
 });
