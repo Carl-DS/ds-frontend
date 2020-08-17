8d0180d963df38d57cef30247b1c5f63aea32ef9
Carl-DS <ldscode@163.com>
2020-08-17 10:10:03 +0800

    doc: over

--- ds-cloud-music/src/application/Player/store/actions.js
+++ ds-cloud-music/src/application/Player/store/actions.js
@@ -9,8 +9,10 @@ import {
   SET_SHOW_PLAYLIST,
   DELETE_SONG,
   INSERT_SONG,
+  CHANGE_SPEED,
 } from "./action-types";
 import { fromJS } from "immutable";
+import { getSongDetailRequest } from "../../../api/request";
 
 export const changeCurrentSong = (data) => ({
   type: SET_CURRENT_SONG,
@@ -42,6 +44,11 @@ export const changePlayMode = (data) => ({
   data,
 });
 
+export const changeSpeed = (data) => ({
+  type: CHANGE_SPEED,
+  data,
+});
+
 export const changeCurrentIndex = (data) => ({
   type: SET_CURRENT_INDEX,
   data,
@@ -52,7 +59,61 @@ export const changeShowPlayList = (data) => ({
   data,
 });
 
+export const insertSong = (data) => ({
+  type: INSERT_SONG,
+  data,
+});
+
 export const deleteSong = (data) => ({
   type: DELETE_SONG,
-  data
+  data,
 });
+
+// const insertSong = (dispatch, getState, song) => {
+//   const playList = JSON.parse(JSON.stringify(getState().getIn(['player', 'playList']).toJS()));
+//   const sequenceList = JSON.parse(JSON.stringify(getState().getIn(['player', 'sequencePlayList']).toJS()));
+//   let currentIndex = getState().getIn(['player', 'currentIndex']);
+//   //看看有没有同款
+//   let fpIndex = findIndex(song, playList);
+//   // 如果是当前歌曲直接不处理
+//   if(fpIndex === currentIndex && currentIndex != -1) return;
+//   currentIndex++;
+//   // 把歌放进去,放到当前播放曲目的下一个位置
+//   playList.splice(currentIndex, 0, song);
+//   console.log(playList)
+//   console.log(currentIndex)
+//   console.log(fpIndex)
+//   // 如果列表中已经存在要添加的歌
+//   if(fpIndex > -1) {
+//     if(currentIndex > fpIndex) {
+//       playList.splice(fpIndex, 1);
+//       currentIndex--;
+//     } else {
+//       playList.splice(fpIndex+1, 1);
+//     }
+//   }
+
+//   let sequenceIndex = findIndex(playList[currentIndex], sequenceList) + 1;
+//   sequenceList.splice(sequenceIndex, 0, song);
+//   let fsIndex = findIndex(song, sequenceList);
+//   if(fsIndex > -1) {
+//     if(currentIndex > fsIndex) {
+//       sequenceList.splice(fsIndex, 1);
+//       currentIndex--;
+//     } else {
+//       sequenceList.splice(fsIndex + 1, 1);
+//     }
+//   }
+//   dispatch(changePlayList(playList));
+//   dispatch(changeSequecePlayList(sequenceList));
+//   dispatch(changeCurrentIndex(currentIndex));
+// }
+
+export const getSongDetail = (id) => {
+  return (dispatch) => {
+    getSongDetailRequest(id).then((data) => {
+      let song = data.songs[0];
+      dispatch(insertSong(song));
+    });
+  };
+};
--- ds-cloud-music/src/layouts/HomeLayout.js
+++ ds-cloud-music/src/layouts/HomeLayout.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
-import { Top, Tab, TabItem } from "./style";
-import Player from "../Player";
+import { Top, Tab, TabItem } from "./HomeLayout.style";
+import { NavLink } from "react-router-dom";
+import Player from "../application/Player/index";
 
 function Home(props) {
   const { route } = props;
+
   return (
     <div>
       <Top>
-        <span className="iconfont menu">&#xe65c;</span>
-        <span className="title">轻音</span>
+        <span
+          className="iconfont menu"
+          onClick={() => alert("用户中心正在开发中，敬请期待:)")}
+        >
+          &#xe65c;
+        </span>
+        <span className="title">云音悦</span>
         <span
           className="iconfont search"
           onClick={() => props.history.push("/search")}
@@ -21,17 +27,17 @@ function Home(props) {
       <Tab>
         <NavLink to="/recommend" activeClassName="selected">
           <TabItem>
-            <span> 推荐 </span>
+            <span>推荐</span>
           </TabItem>
         </NavLink>
         <NavLink to="/singers" activeClassName="selected">
           <TabItem>
-            <span> 歌手 </span>
+            <span>歌手</span>
           </TabItem>
         </NavLink>
         <NavLink to="/rank" activeClassName="selected">
           <TabItem>
-            <span> 排行榜 </span>
+            <span>排行榜</span>
           </TabItem>
         </NavLink>
       </Tab>
